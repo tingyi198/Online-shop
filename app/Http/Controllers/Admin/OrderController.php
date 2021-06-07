92e963f0501c4e2fd99773561ff51feca2a13903
tingyi198 <tingyi198@gmail.com>
2021-06-07 22:04:25 +0800

    install Laravel Debugbar

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Debugbar;
 use App\Models\Order;
 use App\Exports\OrderExport;
 use Illuminate\Http\Request;
@@ -9,6 +10,7 @@ use App\Http\Controllers\Controller;
 use App\Notifications\OrderDelivery;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\OrdersMultipleExport;
+use Symfony\Component\ErrorHandler\Debug;
 
 class OrderController extends Controller
 {
